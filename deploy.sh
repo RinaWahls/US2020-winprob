8d436d35633578bb6467f26507bb9b1ef53f515d
Rina Wahls <catharina.wahls@gmail.com>
2022-12-13 12:03:09 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:connorrothschild/iib-svelte-workshop-chart.git main:gh-pages
+git push -f git@github.com:RinaWahls/US2020-winprob.git main:gh-pages
 
 cd -
